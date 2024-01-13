317cca2b6eda10e4371d39deb6884a9c60bb1385
Lucas Andreatta <drea.pierra@gmail.com>
2024-01-13 18:20:10 -0300

    Base application

---
+++ Api/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Api.Entities;
+
+namespace Api.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
